28cc67003bbe1e4ac3ee1a09d9346538f535f07e
hasegawow <shota.hasegawa0721@gmail.com>
2023-12-15 11:45:48 +0900

    a

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('header.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1", port=8888)
