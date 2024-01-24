49f2a0243a530639120c3097be0682cf1f26080a
橋本怜 <hashimotoren@ip-10-95-3-52.ap-northeast-1.compute.internal>
2024-01-24 10:17:21 +0900

    update login signup

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -10,7 +10,7 @@ class SignupForm(FlaskForm):
     mail_address = StringField('', validators=[DataRequired(), Email()], render_kw={"placeholder":""})
     password = PasswordField('', validators=[DataRequired()], render_kw={"placeholder":""})
     confirm_password = PasswordField('', validators=[DataRequired(), EqualTo('password', message="パスワードが一致しません")], render_kw={"placeholder":""})
-    profile_image = FileField('アカウント画像を選択してください', validators=[FileAllowed(['jpg', 'png'])], render_kw={"placeholder":""})
+    profile_image = FileField('アカウント画像を選択してください', validators=[FileAllowed(['jpg', 'jpeg', 'png'])], render_kw={"placeholder":""})
     submit = SubmitField('登録')
 
 class LoginForm(FlaskForm):
